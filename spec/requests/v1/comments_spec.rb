4dbb4de8f3c60163b3c9697d36254ed6c89ecb62
SarCasm <sarcasm008@gmail.com>
2017-05-23 01:58:37 +0300

    More tests

--- spec/requests/v1/comments_spec.rb
+++ spec/requests/v1/comments_spec.rb
@@ -2,23 +2,38 @@ require 'rails_helper'
 
 RSpec.describe "Comments API", type: :request, version: :v1 do
   let(:user) { FactoryGirl.create(:user_with_projects) }
+  let(:task) { user.projects.first.tasks.first }
+  let(:comment) { task.comments.first }
   let(:other_user) { FactoryGirl.create(:user_with_projects) }
-  let(:comment) { user.projects.first.tasks.first.comments.first }
-  let(:other_comment) { other_user.projects.first.tasks.first.comments.first }
+  let(:other_task) { other_user.projects.first.tasks.first }
+  let(:other_comment) { other_task.comments.first }
 
   describe '#create' do
     context 'when logged in' do
       context 'with valid params' do
+        before { @comment_params = { content: "Some comment about something" } }
+
         it 'creates a new comment' do
-          time = DateTime.now.to_s
-          comment_params = { content: "Some comment about something" }
           auth_post user,
                     comments_path,
-                    params: { task_id: user.projects[0].tasks[0].id, comment: comment_params, format: :json},
+                    params: { task_id: task.id, comment: @comment_params, format: :json},
                     headers: v1_headers
 
           expect(response.status).to eq 200
           expect_json(content: "Some comment about something")
+        end
+
+        context 'editing other user`s comment' do
+          it 'returns 403: Forbidden when accessing others task' do
+            auth_post user,
+                      comments_path,
+                      params: { task_id: other_task.id, comment: @comment_params, format: :json},
+                      headers: v1_headers
+
+            expect(response.status).to eq 403
+            expect(json).to include 'errors'
+            expect(json).to_not include 'name'
+          end
         end
       end
 
