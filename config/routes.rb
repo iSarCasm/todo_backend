ff708381a249e1d8cc763ca82e52576895e66564
SarCasm <sarcasm008@gmail.com>
2017-05-24 18:55:07 +0300

    Move current registrations and ssesion API under V1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    registrations:  'overrides/registrations'
-  }
-
   scope module: :v1, constraints: ApiConstraint.new(version: 1) do
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations:  'overrides/registrations'
+    }
+
     resources :users, only: [:show]
 
     resources :projects, only: [:show, :create, :update, :destroy] do
